b3197411faad7b379139508dfc6a229272959b7b
John Cotterell <Cotterzz@users.noreply.github.com>
2021-10-11 09:27:21 +0100

    added fish to index, coloured coral

--- docs/jsm/fish.js
+++ docs/jsm/fish.js
@@ -39,68 +39,40 @@ controls.target.set(0, 1, 0)
 
 //const material = new THREE.MeshNormalMaterial()
 
-let coral;
-let count = 100;
-let corals = new Array(count);
-let materials = new Array(count);
-let spread = 20;
-let minscale = 1;
-let maxscale = 3;
-let basescale = 0.01;
+let container = new THREE.Mesh();
+scene.add(container);
+let count = 150;
+let worms = new Array(count);
+let animations = new Array(count)
 
-const fbxLoader = new FBXLoader()
-fbxLoader.load(
-urlprefix + 'coral/coral3.fbx',
+let mixer;
+
+const gLoader = new GLTFLoader().setPath( urlprefix );
+gLoader.load(
+    'fish/fish.glb',
     (object) => {
+    	console.log(object);
+        //if ((child as THREE.Mesh).isMesh) {
+        //         // (child as THREE.Mesh).material = material
+        //         if ((child as THREE.Mesh).material) {
+        //             ((child as THREE.Mesh).material as THREE.MeshBasicMaterial).transparent = false
+        //         }
+            // }
+        //});
+        //container.scale.set(.2, .2, .2);
         
-        coral = object;
-        scene.add(coral);
-        const bleachedColor = Math.random();
-        const bleachedRGBColor = new THREE.Color( 1, bleachedColor, bleachedColor );
-
-        var material = new THREE.MeshPhongMaterial( {
-                                //map: imgTexture,
-                                //bumpMap: imgTexture,
-                                //bumpScale: bumpScale,
-                                color: bleachedColor,
-                                //specular: specularColor,
-                                //reflectivity: beta,
-                                //shininess: specularShininess,
-                                //envMap: alphaIndex % 2 === 0 ? null : reflectionCube
-                            } );
-        coral.scale.set(basescale, basescale, basescale);
-        coral.traverse(function (child) {
-                if ( child instanceof THREE.Mesh ) {
-                       console.log(child);
-
-                       child.material = material;
-                        
-                    }
-            
-            });
+        
+        container.add(object.scenes[0]);
         
         for ( let p = 0; p < count; p ++ ) {
-            //corals[p] = SKUTILS.clone(coral);
-            corals[p] = coral.clone();
-            //console.log(corals[p])
-            scene.add(corals[p]);
-            corals[p].position.set((Math.random()*spread)-(spread/2), 0, Math.random()*(-spread));
-            corals[p].rotation.y = Math.random()*6;
-            let scale = minscale + (Math.random()*(maxscale-minscale));
-            corals[p].scale.set(basescale*scale, basescale*scale, basescale*scale);
-            materials[p] = new THREE.MeshPhongMaterial();
-            materials[p].color = new THREE.Color( Math.random(), Math.random(), Math.random() );
-            corals[p].traverse(function (child) {
-               if(p==0){console.log(child);}
-                if ( child instanceof THREE.Mesh ) {
-                        
-                        child.material = materials[p];
-                        
-                        
-                    }
-            });
+            worms[p] = container.clone();
+            scene.add(worms[p]);
+            worms[p].position.set((Math.random()*5)-2.5,  Math.random()*-5, Math.random()*-5);
+            worms[p].rotation.y = Math.random()*6;
+            
         }
         animate();
+        
     },
     (xhr) => {
         console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
@@ -123,7 +95,7 @@ function onWindowResize(event) {
         renderer.setSize(window.innerWidth, window.innerHeight);
         renderer.render(scene, camera);
 }
-//const stats = Stats()
+///const stats = Stats()
 //document.body.appendChild(stats.dom)
 
 function animate() {
